485eafc2ce726a56f346dd2c4baeb3b3ae61d1c3
website-test1 <goblin8067@yandex.ru>
2024-12-12 22:56:41 +0300

    1

---
+++ footer.php
@@ -0,0 +1,61 @@
+      <footer class="footer">
+        <img class="footer-img" src="images/bg-header3.jpg" alt="" />
+        <div class="container">
+          <div class="row justify-content-around">
+            <div class="col-lg-3 col-sm-6">
+              <div class="single-box flex-column d-flex justify-content-center align-items-center">
+                <img src="images/icons/logo.png" width="100" class="d-flex footer-logo" alt="" />
+                <h2 class="text-uppercase d-flex"></h2>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="single-box">
+                <a href="index.html" class="text-decoration-none">
+                  <h2>Главная</h2>
+                </a>
+                <ul>
+                  <li><a href="">Портфолио</a></li>
+                  <li><a href="">Продукция</a></li>
+                  <li><a href="">О нас</a></li>
+                  <li><a href="">Контакты</a></li>
+                  <li>
+                    <button
+                      class="btn btn-outline-success btn-body fw-medium border-2 px-3"
+                      type="submit"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal">
+                      Записаться на курсы
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="single-box">
+                <!-- <h2>Domain</h2> -->
+                <p class="adress-text">г. Волгогрпд, пр. Ленина 69а</p>
+                <p class="adress-text">
+                  График работы: <br />
+                  Пн-Сб с 9:00 до 20:00
+                </p>
+                <a class="text-decoration-none phone-link-navbar phone-link-navbar-footer" href="tel:+79067751938">
+                  <i class="bi-telephone-fill me-2"></i>8(906) 775-19-38</a
+                >
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="single-box">
+                <h2 class="text-uppercase">"мегаполис 34"</h2>
+                <p class="text-uppercase">Произведем и установим любую рекламную конструкцию в срок от 3 дней</p>
+                <h2>Здесь мы всегда на связи</h2>
+                <p class="socials">
+                  <a target="_blank" href="https://m.vk.com/english_indeed"><i class="fa-brands fa-vk"></i></a>
+                  <a target="_blank" href="https://www.youtube.com/@englishindeedschool8012"
+                    ><i class="bi bi-youtube"></i
+                  ></a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
---
+++ navbar.php
@@ -0,0 +1,64 @@
+          <nav class="navbar fixed-top navbar-dark navbar-expand-xl navbar-id p-0 m-0">
+            <div class="container-fluid py-1 px-5">
+              <a class="navbar-brand m-0" href="#">
+                <img class="navbar__logo-img" src="images/icons/logo.png" alt="logo" height="80" />
+              </a>
+              <button
+                class="navbar-toggler shadow-none border-0"
+                type="button"
+                data-bs-target="#navbarNav"
+                data-bs-toggle="collapse"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle Navbar">
+                <span class=""><i class="bi bi-list" style="font-size: 2rem; color: var(--color-1)"></i></span>
+              </button>
+              <div class="collapse navbar-collapse navbar-collapse-id" id="navbarNav">
+                <ul class="navbar-nav mb-2 mb-lg-0 ul-navdar align-items-center justify-content-center w-100 gap-3">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link text-uppercase">Готовые проекты</a>
+                  </li>
+                  <li class="nav-item dropdown text-center">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      ПРОДУКЦИЯ
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Объемные буквы</a></li>
+                      <li><a class="dropdown-item" href="#">Вывеска</a></li>
+                      <li><a class="dropdown-item" href="#">Световой короб</a></li>
+                      <!-- <li><hr class="dropdown-divider" /></li> -->
+                      <li><a class="dropdown-item" href="#">Панель-кронштейны, указатели</a></li>
+                      <li><a class="dropdown-item" href="#">Пилон, стела, сити-формат</a></li>
+                      <li><a class="dropdown-item" href="#">Крышная установка</a></li>
+                      <li><a class="dropdown-item" href="#">Оформление фасада</a></li>
+                    </ul>
+                  </li>
+                  <!-- <li class="nav-item">
+                    <a href="#" class="nav-link text-uppercase">Статьи</a>
+                  </li> -->
+                  <li class="nav-item">
+                    <a href="#" class="nav-link text-uppercase">О нас</a>
+                  </li>
+                  <li class="nav-item me-2">
+                    <a href="contacty.html" class="nav-link text-uppercase">Контакты</a>
+                  </li>
+                </ul>
+                <div class="nav-item-contacts">
+                  <a class="nav-item-contacts-link" href="tel:88442502014"
+                    ><i class="bi bi-telephone-fill me-2"></i>(8442) 50-20-14</a
+                  >
+                  <a class="nav-item-contacts-link" href="tel:+79064002773"
+                    ><i class="bi bi-telephone-fill me-2"></i>8-906-400-27-73</a
+                  >
+                  <a class="nav-item-contacts-link" href="#"
+                    ><i class="bi bi-geo-alt-fill me-2"></i>г.Волгоград, пр.Ленина 69а</a
+                  >
+                </div>
+              </div>
+            </div>
+          </nav>
